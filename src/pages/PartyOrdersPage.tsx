c1519239579c394bb6b3d7345c612afb43544736
Arunkumar132 <arun132003@gmail.com>
2025-12-12 14:08:52 +0530

    Frontend changes complated

--- src/pages/PartyOrdersPage.tsx
+++ src/pages/PartyOrdersPage.tsx
@@ -71,7 +71,6 @@ const PartyOrdersPage: React.FC = () => {
         <section>
           <div className='py-2'>
             <h2 className="text-center text-xl sm:text-2xl font-semibold text-gray-900 mb-10"><span className="text-[#510088]"></span></h2>
-            <img src="/line.png" alt="Curved underline" className="mx-auto w-32 -mt-17" />
 
             <div className="grid gap-4 grid-cols-2 sm:grid-cols-4">
               {offerings.map((o, i) => (
